1f0514c3310d07374a9eda3686eedefa6da2b268
renato76 <renmignogna@gmail.com>
2021-07-23 20:01:45 +0100

    update products and use styled components

---
+++ frontend/components/Product.js
@@ -0,0 +1,12 @@
+import ItemStyles from './styles/ItemStyles';
+
+export default function Product({ product }) {
+  return (
+    <ItemStyles>
+      <img
+        src={product?.photo?.image?.publicUrlTransformed}
+        alt={product.name}
+      />
+    </ItemStyles>
+  );
+}
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,7 +1,1 @@
-export default function IndexPage() {
-  return (
-    <>
-      <p>Index page</p>
-    </>
-  );
-}
+export { default } from './products';
