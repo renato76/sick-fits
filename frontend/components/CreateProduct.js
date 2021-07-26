d501eeb668b9f9107b0e97bb19ca143b45fd1ef9
renato76 <renmignogna@gmail.com>
2021-07-26 09:13:27 +0100

    add navbar, change geader and create starter code for create prodduct

---
+++ frontend/components/CreateProduct.js
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+
+export default function CreateProduct() {
+  const [name, setName] = useState('Wes');
+  return (
+    <form>
+      <label htmlFor="name">
+        Name
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => {
+            console.log(e.target.value);
+            setName(e.target.value);
+          }}
+        />
+      </label>
+    </form>
+  );
+}
