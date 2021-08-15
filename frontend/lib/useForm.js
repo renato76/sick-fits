f1d8aa5646494f80d55cd407a42382c727d48094
renato76 <renmignogna@gmail.com>
2021-08-15 22:47:30 +0100

    add create product mutation

--- frontend/lib/useForm.js
+++ frontend/lib/useForm.js
@@ -1,19 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export default function useForm(initial = {}) {
   // create a state object for our inputs
   const [inputs, setInputs] = useState(initial);
-  const initialValues = Object.values(initial).join('');
-
-  useEffect(() => {
-    // This function runs when the things we are watching change
-    setInputs(initial);
-  }, [initialValues]);
 
   // {
   //   name: 'wes',
   //   description: 'nice shoes',
-  //   price: 100
+  //   price: 1000
   // }
 
   function handleChange(e) {
