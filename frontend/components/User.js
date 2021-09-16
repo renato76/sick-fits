bafedbe5aa719523b273205157602fba86782b77
renato76 <renmignogna@gmail.com>
2021-09-16 12:27:16 +0100

    display cart items in custom component

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -1,12 +1,27 @@
 import { gql, useQuery } from '@apollo/client';
 
-export const CURRENT_USER_QUERY = gql`
+const CURRENT_USER_QUERY = gql`
   query {
     authenticatedItem {
       ... on User {
         id
         email
         name
+        cart {
+          id
+          quantity
+          product {
+            id
+            price
+            name
+            description
+            photo {
+              image {
+                publicUrlTransformed
+              }
+            }
+          }
+        }
       }
     }
   }
@@ -16,3 +31,5 @@ export function useUser() {
   const { data } = useQuery(CURRENT_USER_QUERY);
   return data?.authenticatedItem;
 }
+
+export { CURRENT_USER_QUERY };
