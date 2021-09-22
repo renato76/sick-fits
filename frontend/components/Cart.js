c3a57822213f1b05e729f66f487639189132bf4a
renato76 <renmignogna@gmail.com>
2021-09-22 21:41:23 +0100

    create checkout and edit cart

--- frontend/components/Cart.js
+++ frontend/components/Cart.js
@@ -1,4 +1,3 @@
-/* eslint-disable react/prop-types */
 import styled from 'styled-components';
 import CartStyles from './styles/CartStyles';
 import CloseButton from './styles/CloseButton';
@@ -8,7 +7,7 @@ import { useUser } from './User';
 import calcTotalPrice from '../lib/calcTotalPrice';
 import { useCart } from '../lib/cartState';
 import RemoveFromCart from './RemoveFromCart';
-import Checkout from './Checkout';
+import { Checkout } from './Checkout';
 
 const CartItemStyles = styled.li`
   padding: 1rem 0;
