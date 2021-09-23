cb020f4dcb550843a255affe4aa82b55bcc4e55a
renato76 <renmignogna@gmail.com>
2021-09-23 16:26:55 +0100

    create order and orderitems mutations

--- backend/mutations/checkout.ts
+++ backend/mutations/checkout.ts
@@ -1,3 +1,5 @@
+import { CartItem } from '../schemas/CartItem';
+import { User } from '../schemas/User';
 import {
   CartItemCreateInput,
   OrderCreateInput,
@@ -56,21 +58,45 @@ async function checkout(
   const amount = cartItems.reduce(function(tally: number, cartItem: CartItemCreateInput) {
     return tally + cartItem.quantity * cartItem.product.price;
   }, 0);
-  console.log('amount >>>', amount);
+  console.log(amount);
   // 3. create the charge with the stripe library
   const charge = await stripeConfig.paymentIntents.create({
     amount,
-    currency: 'GBP',
+    currency: 'USD',
     confirm: true,
     payment_method: token,
   }).catch(err => {
     console.log(err);
     throw new Error(err.message);
   });
-
   console.log(charge)
   // 4. Convert the cartItems to OrderItems
+  const orderItems = cartItems.map(cartItem => {
+    const orderItem = {
+      name: cartItem.product.name,
+      description: cartItem.product.description,
+      price: cartItem.product.price,
+      quantity: cartItem.quantity,
+      photo: { connect: { id: cartItem.product.photo.id }},
+    }
+    return orderItem;
+  })
   // 5. Create the order and return it
+  const order = await context.lists.Order.createOne({
+    data: {
+      total: charge.amount,
+      charge: charge.id,
+      items: { create: orderItems },
+      user: { connect: { id: userId }}
+    },
+    resolveFields: false,
+  });
+  // 6. Clean up any old cart item
+  const cartItemIds = cartItems.map(cartItem => cartItem.id);
+  await context.lists.CartItem.deleteMany({
+    ids: cartItemIds
+  });
+  return order;
 }
 
 export default checkout;
