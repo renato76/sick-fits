927b51bb8a67dff90f1296457cacaa9d5f36ca4b
renato76 <renmignogna@gmail.com>
2021-09-23 20:45:54 +0100

    display single order and all orders page

--- frontend/lib/calcTotalPrice.js
+++ frontend/lib/calcTotalPrice.js
@@ -1,6 +1,8 @@
 export default function calcTotalPrice(cart) {
+  console.log(cart);
   return cart.reduce((tally, cartItem) => {
     if (!cartItem.product) return tally; // products can be deleted, but they could still be in your cart
-    return tally + cartItem.quantity * cartItem.product.price;
+    // I added *100 because order total is 2 decimal places out
+    return (tally + cartItem.quantity * cartItem.product.price) * 100;
   }, 0);
 }
